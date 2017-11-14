b4127574869aa243de55e77e7891920e243baccd
stani95 <stani95@users.noreply.github.com>
2017-11-14 15:44:37 +0900

    Symbols

---
+++ symbols.py
@@ -0,0 +1,348 @@
+import math
+
+A=[]
+B=[]
+C=[]
+T=[]
+
+with open("language-training-langA-0", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-1", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-2", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-3", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-4", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-5", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-6", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-7", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-8", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-9", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-10", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-11", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-12", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-13", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-14", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-15", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-16", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-17", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-18", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-19", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-20", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-21", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-22", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-23", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-24", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-25", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-26", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-27", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-28", 'r') as doc:
+    A.append(doc.read())
+
+with open("language-training-langA-29", 'r') as doc:
+    A.append(doc.read())
+
+
+
+with open("language-training-langB-0", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-1", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-2", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-3", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-4", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-5", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-6", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-7", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-8", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-9", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-10", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-11", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-12", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-13", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-14", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-15", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-16", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-17", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-18", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-19", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-20", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-21", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-22", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-23", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-24", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-25", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-26", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-27", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-28", 'r') as doc:
+    B.append(doc.read())
+
+with open("language-training-langB-29", 'r') as doc:
+    B.append(doc.read())
+
+
+
+
+with open("language-training-langC-0", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-1", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-2", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-3", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-4", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-5", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-6", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-7", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-8", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-9", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-10", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-11", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-12", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-13", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-14", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-15", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-16", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-17", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-18", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-19", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-20", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-21", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-22", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-23", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-24", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-25", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-26", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-27", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-28", 'r') as doc:
+    C.append(doc.read())
+
+with open("language-training-langC-29", 'r') as doc:
+    C.append(doc.read())
+
+
+with open("language-test-0", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-1", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-2", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-3", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-4", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-5", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-6", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-7", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-8", 'r') as doc:
+    T.append(doc.read())
+with open("language-test-9", 'r') as doc:
+    T.append(doc.read())
+
+
+matrixA=[[0 for i in range(7)] for j in range(7)]
+matrixB=[[0 for i in range(7)] for j in range(7)]
+matrixC=[[0 for i in range(7)] for j in range(7)]
+letters=['A','o','e','t','p','g','k']
+
+for i in A:
+	for j in range(len(i)-1):
+		matrixA[letters.index(i[j])][letters.index(i[j+1])]+=1
+
+for i in B:
+	for j in range(len(i)-1):
+		matrixB[letters.index(i[j])][letters.index(i[j+1])]+=1
+
+for i in C:
+	for j in range(len(i)-1):
+		matrixC[letters.index(i[j])][letters.index(i[j+1])]+=1
+
+posterior = []
+
+for i in T:
+	loglikelihoodA=0
+	for j in range(len(i)-1):
+		loglikelihoodA+=math.log(float(matrixA[letters.index(i[j])][letters.index(i[j+1])])/float(sum(matrixA[letters.index(i[j])]))+0.00000001)
+	likelihoodA=math.exp(loglikelihoodA)
+	loglikelihoodB=0
+	for j in range(len(i)-1):
+		loglikelihoodB+=math.log(float(matrixB[letters.index(i[j])][letters.index(i[j+1])])/float(sum(matrixB[letters.index(i[j])]))+0.00000001)
+	likelihoodB=math.exp(loglikelihoodB)
+	loglikelihoodC=0
+	for j in range(len(i)-1):
+		loglikelihoodC+=math.log(float(matrixC[letters.index(i[j])][letters.index(i[j+1])])/float(sum(matrixC[letters.index(i[j])]))+0.00000001)
+	likelihoodC=math.exp(loglikelihoodC)
+	summ = likelihoodA+likelihoodB+likelihoodC
+	posterior.append([float(likelihoodA)/float(summ), float(likelihoodB)/float(summ), float(likelihoodC)/float(summ)])
+
+for n,i in enumerate(posterior):
+	if i.index(max(i))==0:
+		print "Test number", n, "is written in language A, with probability", max(i)
+	if i.index(max(i))==1:
+		print "Test number", n, "is written in language B, with probability", max(i)
+	if i.index(max(i))==2:
+		print "Test number", n, "is written in language C, with probability", max(i)
+
